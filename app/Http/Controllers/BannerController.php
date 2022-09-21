28cc147b5e74b30c5d880f735d27d92e947749f1
Saytzeff-D <davidatanda2020@gmail.com>
2022-09-21 14:16:52 +0100

    can now delete banner

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Banner;
+use Illuminate\Support\Facades\DB;
 
-// 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 my git token
 class BannerController extends Controller
 {
     public function setBanner()
     {
             $setBanner = Banner::create(['banner'=>$_FILES['banner']['name'], 'caption'=>request('caption'), 'subCaption'=>request('subCaption')]);
             if ($setBanner) {
-                $upload = move_uploaded_file($_FILES['banner']['tmp_name'], 'C:\Users\DELL\Desktop\Angular Class\hotel-app\src\assets\images\\'. $_FILES['banner']['name']);
+                $upload = move_uploaded_file($_FILES['banner']['tmp_name'], 'C:\Users\DELL\Desktop\Angular Projects\hotel-app\src\assets\images\\'. $_FILES['banner']['name']);
             }
             else{
                 return response()->json('Network Error');
@@ -22,5 +22,12 @@ class BannerController extends Controller
     {
         $allBanner = Banner::get();
         return response()->json($allBanner);
+    }
+    public function deleteBanner()
+    {
+        $delete = DB::delete('delete from banners where banner_id = ?', [request('id')]);
+        if($delete){
+            return response()->json('Success', 200);
+        }
     }
 }
