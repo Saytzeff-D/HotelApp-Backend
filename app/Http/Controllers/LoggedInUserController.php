3dde777c81cdb0cdeb304d578fab5af6f322f1f7
Saytzeff-D <davidatanda2020@gmail.com>
2022-09-21 14:17:35 +0100

    awaiting cloudinary, uploading tentatively

--- app/Http/Controllers/LoggedInUserController.php
+++ app/Http/Controllers/LoggedInUserController.php
@@ -11,27 +11,27 @@ class LoggedInUserController extends Controller
     public function __construct()
     {
         $this->middleware('jwt');
-        \Cloudinary::config([
-            "cloud_name" => "ololadedavid15",
-            "api_key" => "693129952828152",
-            "api_secret" => "WIh0n7eDm3wejhDYzjYsfxQbTpI",
-            "secure" => true]);
+        // \Cloudinary::config([
+        //     "cloud_name" => "ololadedavid15",
+        //     "api_key" => "693129952828152",
+        //     "api_secret" => "WIh0n7eDm3wejhDYzjYsfxQbTpI",
+        //     "secure" => true]);
     }
     public function editProfile()
     {
         if($_FILES){
             $user = User::find(auth()->user()->user_id);
-            \Cloudinary\Uploader::upload(
-                "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-                ["public_id" => "olympic_flag"]
-            );
+            // \Cloudinary\Uploader::upload(
+            //     "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+            //     ["public_id" => "olympic_flag"]
+            // );
             $user->profilePic = $_FILES['userPic']['name'];
-            // $updatePic = $user->save();
-            // if($updatePic){
-                // move_uploaded_file($_FILES['userPic']['tmp_name'], 'C:\Users\DELL\Desktop\Angular Class\hotel-app\src\assets\images\\'. $_FILES['userPic']['name']);
+            $updatePic = $user->save();
+            if($updatePic){
+                move_uploaded_file($_FILES['userPic']['tmp_name'], 'C:\Users\DELL\Desktop\Angular Projects\hotel-app\src\assets\images\\'. $_FILES['userPic']['name']);
                 return response()->json('Updated');
-            // }
-            // return response()->json('File Upload Error');
+            }
+            return response()->json('File Upload Error');
         }
         else{
             return response()->json('The server cannot process your request');
